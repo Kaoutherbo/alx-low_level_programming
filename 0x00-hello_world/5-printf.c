7c6331b429614c187cae7528edf2b177bbde3724
kaouther <kaoutherboutheldja@gmail.com>
2023-10-05 07:51:29 -0700

    print paragraph using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
