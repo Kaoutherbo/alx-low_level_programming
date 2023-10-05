90e4a8691f2f31f43271dd4d02579d4d1e526366
kaouther <kaoutherboutheldja@gmail.com>
2023-10-05 07:49:23 -0700

    print paragraph

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - Entry point
+ * main - this is the main function
  *
- * Return: Always 0 (Success)
+ * Return: always 0
  */
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");	
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
