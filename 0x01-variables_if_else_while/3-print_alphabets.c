9af87db00e858594493899f5a9a536017eeac477
kaouther <kaoutherboutheldja@gmail.com>
2023-10-06 07:10:52 -0700

    prints the alphabet in lowercase, and then in uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,8 @@ int main(void)
 
 	for (i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
-	
 	for (i = 'A' ; i <= 'Z' ; i++)
-                putchar(i);
-
+		putchar(i);
 	putchar('\n');
-
 	return (0);
 }
