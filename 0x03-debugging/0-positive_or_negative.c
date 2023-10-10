bac899f1648a3b792fee4c9d1cd982c2e51efdb8
kaouther <kaoutherboutheldja@gmail.com>
2023-10-10 09:50:23 -0700

    this main file is a test for a postitive_or_negative() function similar to the one you worked with in an earlier C project

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * positive_or_negative - prints if integer is positive or negative
+ * @n: parameter to be chacked
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int positive_or_negative(int n)
+{
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	} else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
