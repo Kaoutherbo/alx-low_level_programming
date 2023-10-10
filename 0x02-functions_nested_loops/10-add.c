5db225dc810dac12d8baa7551956661d7a2d02fd
kaouther <kaoutherboutheldja@gmail.com>
2023-10-10 08:01:30 -0700

    adds two integers and returns the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,13 @@
 
 /**
  * add - function that adds two integers and returns the result.
- * @num1: parameter to be checked
- * @num2: parameter to be checked
+ * @a: parameter to be checked
+ * @b: parameter to be checked
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
-int add(int num1 , int num2)
+int add(int a, int b)
 {
-	return (num1+num2);
+	return (a + b);
 }
