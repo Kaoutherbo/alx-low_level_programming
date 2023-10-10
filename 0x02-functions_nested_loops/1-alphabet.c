31e49969fc440df057bdb84f7713f7ca3e261f03
kaouther <kaoutherboutheldja@gmail.com>
2023-10-10 07:12:11 -0700

    prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,12 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 void print_alphabet(void)
 {
 	char i;
-        for(i = 'a' ; i <= 'z' ; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
 
-}	
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
