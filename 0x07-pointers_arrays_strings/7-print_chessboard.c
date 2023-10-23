970dd857068adc1cfeed2b3d9d1b7bd4896e9825
kaouther <kaoutherboutheldja@gmail.com>
2023-10-23 09:01:50 -0700

    prints the chessboard.

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,24 +4,18 @@
  * _print_chessboard -  function that prints the chessboard.
  * @a: Prameter to be checked
  *
- * Description: This function takes a 2D array 'a' representing a chessboard
- * configuration and prints it to the standard output.
- * The chessboard is printed
- * as a grid of characters, with each cell represented by a space or a chess
- * piece character.
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+int i, j;
 
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-		_putchar('\n');
-	}
+for (i = 0 ; i < 8 ; i++)
+{
+for (j = 0 ; j < 8 ; j++)
+    _putchar(a[i][j]);
+_putchar('\n');
+}
 }
