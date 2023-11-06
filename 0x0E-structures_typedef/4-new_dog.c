12e11f6da52812cec5ddeb0fe45dbe6b4728358e
kaouther <kaoutherboutheldja@gmail.com>
2023-11-06 07:51:58 -0800

    creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,36 +1,44 @@
 #include "dog.h"
-#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * new_dog - function that creates a new dog.
- * @name:name of he dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: pointer to dog_t
+ * @name: name of a dog
+ * @age: age of a dog
+ * @owner: owner of dog
+ * Return: pointer to new dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *newDog = (dog_t *)malloc(sizeof(dog_t));
+	dog_t *newDog;
+	int len1, len2;
 
-	if (!newDog)
+	len1 = strlen(name);
+	len2 = strlen(owner);
+
+	newDog = malloc(sizeof(dog_t));
+	if (newDog == NULL)
 		return (NULL);
-	newDog->name = strdup(name);
-	if (!newDog->name)
+
+	newDog->name = malloc(sizeof(char) * len1 + 1);
+	if (newDog->name == NULL)
 	{
 		free(newDog);
 		return (NULL);
 	}
-	newDog->age = age;
-	newDog->owner = strdup(owner);
-	if (!newDog->owner)
+	newDog->owner = malloc(sizeof(char) * len2 + 1);
+	if (newDog->owner == NULL)
 	{
 		free(newDog->name);
 		free(newDog);
 		return (NULL);
 	}
+	strcpy(newDog->name, name);
+	strcpy(newDog->owner, owner);
+	newDog->age = age;
+
 	return (newDog);
 }
-
