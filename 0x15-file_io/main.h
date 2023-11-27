f6cfc8174985ec1a109aeb2ef38ce9346975eff8
kaouther <kaoutherboutheldja@gmail.com>
2023-11-27 15:44:23 +0100

    first commit

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif /* MAIN_H */
